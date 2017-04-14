3bf482e62079452e30d5473f6aa4ca7cd3fcbd5f
Thomas Wraback <twraback3@gatech.edu>
2017-04-14 02:45:44 -0400

    Added all of the activity layouts we should need for the demo.

---
+++ app/src/main/java/twotom/bookhub/BookSearch.java
@@ -0,0 +1,13 @@
+package twotom.bookhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_search);
+    }
+}
---
+++ app/src/main/java/twotom/bookhub/MyBooksActivity.java
@@ -0,0 +1,13 @@
+package twotom.bookhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_books);
+    }
+}
