bf76c5a12de26bdf0009a65967ca77757fedb877
Kijjakarn Praditukrit <kijjakarn_boss@hotmail.com>
2017-09-25 19:29:56 -0400

    Added UI for viewing profile

--- app/src/main/java/twotom/bookhub/ProfileActivity.java
+++ app/src/main/java/twotom/bookhub/ProfileActivity.java
@@ -2,12 +2,89 @@ package twotom.bookhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private boolean isChangingPassword = false;
+    private EditText oldPasswordView;
+    private EditText newPasswordView;
+    private Button passwordButton;
+    private String oldPassword = "";
+    private String newPassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        TextView usernameView = (TextView)
+            findViewById(R.id.textView_profile_username);
+        EditText emailView = (EditText)
+            findViewById(R.id.editText_profile_email);
+        oldPasswordView = (EditText)
+            findViewById(R.id.editText_profile_oldPassword);
+        newPasswordView = (EditText)
+            findViewById(R.id.editText_profile_newPassword);
+        passwordButton = (Button)
+            findViewById(R.id.button_profile_password);
+
+        UserManager userManager = new UserManager();
+        User currentUser = userManager.getCurrentUser();
+
+        usernameView.setText(currentUser.getUsername());
+        emailView.setText(currentUser.getEmail());
+
+        oldPasswordView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence,
+              int i, int i1, int i2) { }
+            @Override
+            public void onTextChanged(CharSequence charSequence,
+              int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                oldPassword = oldPasswordView.getText().toString();
+            }
+        });
+        newPasswordView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence,
+              int i, int i1, int i2) { }
+            @Override
+            public void onTextChanged(CharSequence charSequence,
+              int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                newPassword = newPasswordView.getText().toString();
+            }
+        });
+    }
+
+    public void onChangePassword(View view) {
+        isChangingPassword = !isChangingPassword;
+        if (isChangingPassword) {
+            passwordButton.setText("Cancel");
+            oldPasswordView.setVisibility(View.VISIBLE);
+            newPasswordView.setVisibility(View.VISIBLE);
+        }
+        else {
+            passwordButton.setText("Change Password");
+            oldPasswordView.setVisibility(View.GONE);
+            newPasswordView.setVisibility(View.GONE);
+            oldPassword = "";
+            newPassword = "";
+        }
+    }
+
+    public void onSubmit(View view) {
+        // TODO: Perform network requests here
     }
 }
