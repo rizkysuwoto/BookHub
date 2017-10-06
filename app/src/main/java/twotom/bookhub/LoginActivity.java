e7fd5503d834d5fac70cd1a7078323a7752c5969
rizkysuwoto <rizky_25@live.com>
2017-10-06 18:58:20 -0400

    Deleted Firebase stuff and added menu icons (#30)

--- app/src/main/java/twotom/bookhub/LoginActivity.java
+++ app/src/main/java/twotom/bookhub/LoginActivity.java
@@ -17,11 +17,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
