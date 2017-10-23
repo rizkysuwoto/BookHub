de8fb6334ade193f5f98e0ff2a7b0c34e0e2f355
Kijjakarn Praditukrit <kijjakarn_boss@hotmail.com>
2017-10-22 23:47:36 -0400

    Cleaned up login screen code

--- app/src/main/java/twotom/bookhub/LoginActivity.java
+++ app/src/main/java/twotom/bookhub/LoginActivity.java
@@ -2,22 +2,16 @@ package twotom.bookhub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,7 +28,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLogin(View view) {
-        //TODO: Check Database for proper username/password pair
         CookieHandler.setDefault(new CookieManager());
 
         JSONObject requestBody = new JSONObject();
@@ -42,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
             requestBody.putOpt("username", ((EditText) findViewById(R.id.text_login_username)).getText().toString());
             requestBody.putOpt("password", ((EditText) findViewById(R.id.text_login_password)).getText().toString());
         } catch (JSONException e) {
-            //TODO: handle error
+            Log.e("Error", e.getMessage());
         }
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = NetworkConfiguration.getURL() + "login";
@@ -68,21 +61,11 @@ public class LoginActivity extends AppCompatActivity {
                     Log.e("Error", e.getMessage());
                 }
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Error: ", error.getMessage());
-            }
-        });
+        }, Utilities.getErrorListener(this));
         queue.add(jsObjRequest);
-
-
-
     }
 
     public void onNewUser(View view) {
-        //TODO: Switch to Registration Screen
-
         Intent myIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
         startActivity(myIntent);
     }
