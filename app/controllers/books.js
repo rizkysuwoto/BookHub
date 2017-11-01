9584d6efe60025aabb8e883f4ad5eab7b25898a3
Kijjakarn Praditukrit <kijjakarn_boss@hotmail.com>
2017-11-01 11:44:41 -0400

    Updated books.js to use async/await

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,107 +1,73 @@
 var Book = require('../models/book');
 var Transaction = require('../models/transaction');
-var request = require('request');
-var async = require('async');
+const request = require('request-promise-native');
 
 module.exports = {};
 
-function cacheBook(isbn, callback) {
-    var tasks = [
-        // Look up the book
-        function(callback) {
-            var url = 'https://openlibrary.org/api/books?bibkeys=ISBN:'
-                    + isbn + '&format=json&jscmd=data';
-            request(url, function(err, res, body) {
-                if (err || res.statusCode != 200) {
-                    return callback(new Error('Failed to look up book'));
-                }
-                try {
-                    var book = JSON.parse(body)['ISBN:' + isbn];
-                }
-                catch (err) {
-                    return callback(err);
-                }
-                var newBook = new Book()
-                newBook.title = book.title ? book.title : '';
-                newBook.publisher = book.publisher ? book.publisher : '';
-                try {
-                    newBook.author = book.authors[0].name;
-                }
-                catch (err) {
-                    newBook.author = '';
-                }
-                try {
-                    newBook.isbn10 = book.identifiers.isbn_10[0];
-                }
-                catch (err) {
-                    newBook.isbn10 = isbn;
-                }
-                try {
-                    newBook.isbn13 = book.identifiers.isbn_13[0];
-                }
-                catch (err) {
-                    newBook.isbn13 = '';
-                }
-                var imageURL;
-                try {
-                    imageURL = book.cover.large;
-                }
-                catch (err) {
-                    imageURL = '';
-                }
-                callback(null, newBook, imageURL);
-            });
-        },
-        // Download the thumbnail, then cache the book
-        function(newBook, imageURL, callback) {
-            request.defaults({encoding: null})(
-              imageURL, function(err, res, body) {
-                if (!err && res.statusCode == 200) {
-                    var buffer = new Buffer(body);
-                    newBook.thumbnail = buffer.toString('base64');
-                }
-                else {
-                    newBook.thumbnail = '';
-                }
-                newBook.save(function(err) {
-                    if (err) {
-                        return callback(err);
-                    }
-                    console.log('New book with ISBN', isbn, 'cached');
-                    callback(null, newBook);
-                });
-            });
-        },
-    ];
-    async.waterfall(tasks, callback);
-}
+async function getBook(isbn) {
+    let book = await Book.findOne({$or: [{isbn10: isbn}, {isbn13: isbn}]});
 
-function getBook(isbn, callback) {
-    Book.findOne({$or: [{isbn10: isbn}, {isbn13: isbn}]},
-      function(err, book) {
-        if (err) {
-            return callback(err, null);
-        }
+    // The book has been cached
+    if (book) {
+        return book;
+    }
+
+    // Look up the book
+    const url = 'https://openlibrary.org/api/books?bibkeys=ISBN:'
+              + isbn + '&format=json&jscmd=data';
+    try {
+        const response = await request(url);
+        book = JSON.parse(response)['ISBN:' + isbn];
         if (!book) {
-            return cacheBook(isbn, callback);
+            throw {};
         }
-        callback(null, book);
-    });
-}
+    }
+    catch (err) {
+        throw new Error('Failed to look up book');
+    }
 
-function getBookAsync(isbn) {
-    return new Promise((resolve, reject) => {
-        getBook(isbn, (err, newBook) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(newBook);
-        });
-    });
-};
+    // Create a new Book instance
+    const newBook = new Book();
+    newBook.title = book.title ? book.title : '';
+    newBook.publisher = book.publisher ? book.publisher : '';
+    try {
+        newBook.author = book.authors[0].name;
+    }
+    catch (err) {
+        newBook.author = '';
+    }
+    try {
+        newBook.isbn10 = book.identifiers.isbn_10[0];
+    }
+    catch (err) {
+        newBook.isbn10 = isbn;
+    }
+    try {
+        newBook.isbn13 = book.identifiers.isbn_13[0];
+    }
+    catch (err) {
+        newBook.isbn13 = '';
+    }
+
+    // If the image URL is available, try to download it
+    try {
+        const imageURL = book.cover.large;
+        const response = await request.defaults({encoding: null})(imageURL);
+        const buffer = new Buffer(response);
+        newBook.thumbnail = buffer.toString('base64');
+    }
+    catch (err) {
+        newBook.thumbnail = '';
+    }
+
+    await newBook.save();
+    console.log('New book with ISBN', isbn, 'cached');
+
+    return newBook;
+}
 
 async function getBookCheckTransaction(isbn, username) {
-    var book = await getBookAsync(isbn);
+    const book = await getBook(isbn);
     book.items = await Promise.all(book.items.map(async item => {
         var transaction = await Transaction.findOne(
             {buyer: username, book: book._id}
